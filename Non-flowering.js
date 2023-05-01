d0631cd407ecf31c358ae230958e2c4bc5dc5e0f
AtharvaLitake <112816126+AtharvaLitake@users.noreply.github.com>
2023-05-01 18:57:04 +0530

    FIRST COMMIT

---
+++ Non-flowering.js
@@ -0,0 +1,30 @@
+let http = new XMLHttpRequest();
+
+http.open("get", "Non-flowering.json", true);
+
+http.send();
+
+http.onload = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let flowering = JSON.parse(this.responseText);
+
+    let output = "";
+
+    for (let item of flowering) {
+      output += `
+            <div class = "flowers">
+                <div class ="flower-images">
+                <img src="${item.image}" alt="${item.image}">
+                </div>
+                <h2 class ="title">${item.title}</h2>
+                <hr>
+                <div class ="price">
+                <p>${item.price}<p>
+                </div>
+                <button> View Item &nbsp;&nbsp;&nbsp;&nbsp;<i class="fa-regular fa-eye"></i> </button>
+            </div>
+            `;
+    }
+    document.querySelector(".products").innerHTML = output;
+  }
+};
---
+++ top-products.js
@@ -0,0 +1,30 @@
+let http = new XMLHttpRequest();
+
+http.open("get", "Top_products.json", true);
+
+http.send();
+
+http.onload = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let flowering = JSON.parse(this.responseText);
+
+    let output = "";
+
+    for (let item of flowering) {
+      output += `
+            <div class = "flowers">
+                <div class ="flower-images">
+                <img src="${item.image}" alt="${item.image}">
+                </div>
+                <h2 class ="title">${item.title}</h2>
+                <hr>
+                <div class ="price">
+                <p>${item.price}<p>
+                </div>
+                <a href="${item.link}"><button> View Item &nbsp;&nbsp;&nbsp;&nbsp;<i class="fa-regular fa-eye"></i> </button></a>
+            </div>
+            `;
+    }
+    document.querySelector(".products").innerHTML = output;
+  }
+}
